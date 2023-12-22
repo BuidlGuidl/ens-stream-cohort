740d0e88147cc8ee402a0244605d0950313dbb81
viraj124 <virajm72@gmail.com>
2023-12-22 11:00:37 +0530

    refactor: gas golfing

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -10,6 +10,12 @@ contract ENSContract {
 	}
 }
 
+// ERRORS
+error ARRAY_LENGTH_MISMATCH();
+error NO_ACTIVE_STREAM();
+error NOT_ENOUGH_FUNDS();
+error TRANSFER_FAILED();
+
 contract YourContract is Ownable {
 
     ENSContract public immutable ensContract = ENSContract(0xa58E81fe9b61B5c3fE2AFD33CF304c454AbFc7Cb);
@@ -23,14 +29,12 @@ contract YourContract is Ownable {
         uint256 last;
     }
     mapping(address => BuilderStreamInfo) public streamedBuilders;
-    // ToDo. Change to 30 days
-    uint256 public frequency = 2592000; // 30 days
+
+    uint256 public constant frequency = 2592000; // 30 days
 
     event Withdraw(address indexed to, uint256 amount, string reason);
     event AddBuilder(address indexed to, uint256 amount);
     event UpdateBuilder(address indexed to, uint256 amount);
-
-    constructor() { }
 
     struct BuilderData {
         address builderAddress;
@@ -38,19 +42,23 @@ contract YourContract is Ownable {
         uint256 unlockedAmount;
     }
 
-    function allBuildersData(address[] memory _builders) public view returns (BuilderData[] memory) {
-        BuilderData[] memory result = new BuilderData[](_builders.length);
-        for (uint256 i = 0; i < _builders.length; i++) {
+    function allBuildersData(address[] calldata _builders) public view returns (BuilderData[] memory) {
+        uint256 _length = _builders.length;
+        BuilderData[] memory result = new BuilderData[](_length);
+        for (uint256 i; i < _length;) {
             address builderAddress = _builders[i];
             BuilderStreamInfo storage builderStream = streamedBuilders[builderAddress];
             result[i] = BuilderData(builderAddress, builderStream.cap, unlockedBuilderAmount(builderAddress));
+            unchecked {
+                ++i;
+            }
         }
         return result;
     }
 
     function unlockedBuilderAmount(address _builder) public view returns (uint256) {
         BuilderStreamInfo memory builderStream = streamedBuilders[_builder];
-        require(builderStream.cap > 0, "No active stream for builder");
+        if (builderStream.cap == 0) revert NO_ACTIVE_STREAM();
 
         if (block.timestamp - builderStream.last > frequency) {
             return builderStream.cap;
@@ -64,27 +72,31 @@ contract YourContract is Ownable {
         emit AddBuilder(_builder, _cap);
     }
 
-    function addBatch(address[] memory _builders, uint256[] memory _caps) public onlyOwner {
-        require(_builders.length == _caps.length, "Lengths are not equal");
-        for (uint256 i = 0; i < _builders.length; i++) {
+    function addBatch(address[] calldata _builders, uint256[] calldata _caps) public onlyOwner {
+        if (_builders.length != _caps.length) revert ARRAY_LENGTH_MISMATCH();
+        for (uint256 i; i < _builders.length;) {
             addBuilderStream(payable(_builders[i]), _caps[i]);
+            unchecked {
+                ++i;
+            }
         }
     }
 
     function updateBuilderStreamCap(address payable _builder, uint256 _cap) public onlyOwner {
         BuilderStreamInfo memory builderStream = streamedBuilders[_builder];
-        require(builderStream.cap > 0, "No active stream for builder");
+        if (builderStream.cap == 0) revert NO_ACTIVE_STREAM();
         streamedBuilders[_builder].cap = _cap;
         emit UpdateBuilder(_builder, _cap);
     }
 
     function streamWithdraw(uint256 _amount, string memory _reason) public {
-        require(address(this).balance >= _amount, "Not enough funds in the contract");
+        if (address(this).balance < _amount) revert NOT_ENOUGH_FUNDS();
         BuilderStreamInfo storage builderStream = streamedBuilders[msg.sender];
-        require(builderStream.cap > 0, "No active stream for builder");
+
+        if (builderStream.cap == 0) revert NO_ACTIVE_STREAM();
 
         uint256 totalAmountCanWithdraw = unlockedBuilderAmount(msg.sender);
-        require(totalAmountCanWithdraw >= _amount,"Not enough in the stream");
+        if (totalAmountCanWithdraw < _amount) revert NOT_ENOUGH_FUNDS();
 
         uint256 cappedLast = block.timestamp - frequency;
         if (builderStream.last < cappedLast){
@@ -94,12 +106,11 @@ contract YourContract is Ownable {
         builderStream.last = builderStream.last + ((block.timestamp - builderStream.last) * _amount / totalAmountCanWithdraw);
 
         (bool sent,) = msg.sender.call{value: _amount}("");
-        require(sent, "Failed to send Ether");
+        if (!sent) revert TRANSFER_FAILED();
 
         emit Withdraw(msg.sender, _amount, _reason);
     }
 
     // to support receiving ETH by default
     receive() external payable {}
-    fallback() external payable {}
 }
